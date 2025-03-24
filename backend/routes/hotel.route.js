9c57481c07196a646fba86532ae69059c6af22b4
Nethra-Balan <nethrabalan2005@gmail.com>
2025-03-25 00:52:27 +0530

    Initial commit

--- backend/routes/hotel.route.js
+++ backend/routes/hotel.route.js
@@ -1,4 +1,5 @@
 const express=require('express');
+<<<<<<< HEAD
 const {getHotels,getHotelByCity,getHotelById,postHotel,updateHotel,deleteHotel,getHotelPhotosById,getHotelAmenitiesById,postAdmin,loginAdmin} = require('../controller/hotel.controller');
 const router=express.Router();
 
@@ -14,5 +15,15 @@ router.get('/amenities/:id',getHotelAmenitiesById);
 
 router.post('/admin',postAdmin);
 router.post('/admin/login',loginAdmin);
+=======
+const {getHotels,getHotelByCity,postHotel,updateHotel,deleteHotel} = require('../controller/hotel.controller');
+const router=express.Router();
+
+router.get('/',getHotels);
+router.get('/:city',getHotelByCity);
+router.post('/',postHotel);
+router.put('/:id',updateHotel);
+router.delete('/:id',deleteHotel);
+>>>>>>> f7d7c27 (Initial commit)
 
 module.exports=router;
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const hotelRoute = require('./routes/hotel.route');
+<<<<<<< HEAD
 const userRoute = require('./routes/user.route');
+=======
+>>>>>>> f7d7c27 (Initial commit)
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+<<<<<<< HEAD
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/hotels', hotelRoute);
 app.use('/api/users',userRoute);
+=======
+
+app.use('/api/hotels', hotelRoute);
+>>>>>>> f7d7c27 (Initial commit)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
